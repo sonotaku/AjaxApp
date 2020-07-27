11ade339bc3db1ccf62eb6e75edc9c35bd8650f1
sonotaku <takumasonoda0223@icloud.com>
2020-07-27 11:49:49 +0900

    サーバーの既読機能設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,22 @@ class PostsController < ApplicationController
   end
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
 
+  def checked
+    # URLパラメーターから、既読したメモのidが渡されるよう設定、そのidを使用し該当するレコードを取得
+    # 既読してる→既読を解除するためにfalseへ変更
+    # 既読してない→既読にするためtrueへ変更
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+    # 更新したレコードを取得し直しJSON形式（データ）としてchecked.jsに返却
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
 
